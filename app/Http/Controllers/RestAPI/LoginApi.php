4366aed826b41a1356d99f5a2e74c3dc3fdfba23
Lil Zulf <najibzulfan@gmail.com>
2023-10-25 09:47:13 +0700

    (+) Login Register

---
+++ app/Http/Controllers/RestAPI/LoginApi.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Http\Controllers\RestAPI;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class LoginApi extends Controller
+{
+    /**
+     * Handle the incoming request.
+     */
+    public function __invoke(Request $request)
+    {
+        //
+    }
+}
